1c736cfe5a1223aafc20bb9b9f5a850d0928c12e
thetrashed <>
2023-09-19 23:12:10 +0500

    Cleaned cargo clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use image::GenericImageView;
-use terminal_size;
+use terminal_size::terminal_size;
 
 use std::env;
 use std::path::Path;
@@ -15,25 +15,23 @@ fn main() {
     let characters =
         String::from("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$");
 
-    let (term_width, term_height) = terminal_size::terminal_size().unwrap();
+    let (term_width, term_height) = terminal_size().unwrap();
     let mut img_width = img.dimensions().0;
     let mut img_height = img.dimensions().1;
 
     if (term_width.0 as u32) < img_width {
         img = img.resize(term_width.0 as u32, img_height, image::imageops::Gaussian);
-    } 
-
-    img_width = img.dimensions().0;
-    img_height = img.dimensions().1;
-    if (term_height.0 as u32) < img_height {
-            img = img.resize(img_width, term_height.0 as u32, image::imageops::Gaussian);
+        img_width = img.dimensions().0;
+        img_height = img.dimensions().1;
     }
 
-    img_width = img.dimensions().0;
+    if (term_height.0 as u32) < img_height {
+        img = img.resize(img_width, term_height.0 as u32, image::imageops::Gaussian);
+        img_width = img.dimensions().0;
+    }
 
     let brightness: Vec<_> = img
         .pixels()
-        .into_iter()
         .map(|x| ((x.2[0] as u16 + x.2[1] as u16 + x.2[2] as u16) / 3) as u8)
         .collect();
 
@@ -42,14 +40,12 @@ fn main() {
         .map(|&x| characters.chars().nth((x as f64 / 20.0).round() as usize))
         .collect();
 
-    let mut count = 0;
-    for pixel in converted_pixels {
-        if count % img_width == 0 && count != 0 {
+    for (count, pixel) in converted_pixels.iter().enumerate() {
+        if count as u32 % img_width == 0 && count != 0 {
             print!("\n{}{}{}", pixel.unwrap(), pixel.unwrap(), pixel.unwrap());
         } else {
             print!("{}{}{}", pixel.unwrap(), pixel.unwrap(), pixel.unwrap());
         }
-        count += 1;
     }
     println!();
 }
